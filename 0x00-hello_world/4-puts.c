d567a545eceb7a50ed216ead8021f5331b56bad5
adebsa2401 <adebsa2401@gmail.com>
2022-10-13 21:58:39 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+  * main - puts
+  * Return: 0
+  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
