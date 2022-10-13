0442975718437905ab4d8b388c64a35ad1c3e83d
adebsa2401 <adebsa24012gmail.com>
2022-10-13 13:46:33 -0700

    fix error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main description
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
- return (1);
+return (1);
 }
