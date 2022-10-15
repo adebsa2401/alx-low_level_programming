4cd7da883c2271e7dfc3e0c3892e2f2751934467
adebsa2401 <adebsa24012gmail.com>
2022-10-14 18:17:58 -0700

    fix typos

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,12 @@
  */
 int main(void)
 {
-  char ch;
+char ch;
 
-  for (ch = 'a'; ch <= 'z'; ch++)
-  {
-    putchar(ch);
-  }
-  putchar('\n');
-  return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
