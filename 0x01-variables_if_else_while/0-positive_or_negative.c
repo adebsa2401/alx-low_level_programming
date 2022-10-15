9daf785d6e3c67afe0135321b2fa1ef89fe86a7b
adebsa2401 <adebsa24012gmail.com>
2022-10-14 18:15:01 -0700

    fix typos

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-  int n;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  /* your code goes there */
-  if (n > 0)
-    printf("%d is positive\n", n);
-  else if (n == 0)
-    printf("%d is zero\n", n);
-  else if (n < 0)
-    printf("%d is negative\n", n);
-  return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+return (0);
 }
