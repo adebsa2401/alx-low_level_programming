8ce29f82d1fa59b02ae384ebab03ce2f737055b7
Ben Sales <adebsa2401@gmail.com>
2022-12-02 00:15:09 +0100

    fix typos

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * binary_to_uint - binary to uint
  * @b: char*
- * Return: 0
+ *
+ * Return: int
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int res = 0;
-int base = 1, i = 0;
-if (b == NULL)
-return (0);
-while (b[i + 1])
-{
-if (b[i] != '0' && b[i] != '1')
-return (0);
-i++;
-}
-while (i >= 0)
-{
-res += ((b[i] - '0') * base);
-base *= 2;
-i--;
-}
-return (res);
+	unsigned int num, i;
+
+	if (!b || !*b)
+		return (0);
+
+	num = i = 0;
+	while (b[i])
+	{
+		if (b[i] > 49)
+			return (0);
+		else if (b[i] == 49)
+		{
+			num <<= 1;
+			num += 1;
+		}
+		else
+			num <<= 1;
+		i++;
+	}
+	return (num);
 }
