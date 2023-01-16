18e4a835f6faaf8cb02b913c1d58fd4752ed2a14
Ben Sales <adebsa2401@gmail.com>
2023-01-16 19:25:01 +0100

    feat: Change lib url

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/B3zaleel/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib101-makemewin.so
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/adebsa2401/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101-makemewin.so?raw=true
 export LD_PRELOAD=$HOME/lib101-makemewin.so
